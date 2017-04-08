591d69520546635ba9fe1f531b32962bc127fd8c
Logan Saruwatari <lsaruwatari@gmail.com>
2017-04-08 17:08:48 -0600

    finished the final section to handle multi-part form data. Pretty good learning experience. I expect to reference a lot of this code for future node projects.

--- server.js
+++ server.js
@@ -11,17 +11,7 @@ function start(route, handle){
 		
 		console.log("path name: " + pathname);
 
-		request.setEncoding("utf8");
-
-		request.addListener("data", function(postDataChunk) {
-			postData += postDataChunk;
-			console.log("Received POST data chunk '"+
-			postDataChunk + "'.");
-		});
-
-		request.addListener("end", function() {
-			route(handle, pathname, response, postData);
-		});
+		route(handle, pathname, request, response);
 
 	}).listen(8888);
 
